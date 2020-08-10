aab8919b6ef92c2441ae3f6194a468ec6206cd7d
Starboy-Sharma <pankajsharmavats88@gmail.com>
2020-08-10 21:27:40 +0530

    using react service worker

--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
 
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-/**
- * @Todos
- * [] Create a custome hook
- * [] On click change temprature from F to C
- * [] Write a Readme
- * [] Run app in offline state as well
- * [] About me page add.
- */
-
-/**
- * App Feature
- * 1. Animated Icons of weather
- * 2. Temprature from F to C.
- * 3. Dynamic Background Images
- * 4. Show weather report using user geolocation
- * 5. Web can be run in offline state as well.
- */
+serviceWorker.register();
